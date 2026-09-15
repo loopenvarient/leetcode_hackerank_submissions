c8eb47f4d0b93f159ff0eeb51805980533d878c0
Armeen Asim <armeen.aasim@gmail.com>
2026-09-15 09:27:32 +0500

    [LeetCode][Medium] 0008-string-to-integer-atoi - Time: 0 ms (100%), Space: 19.4 MB (13.33%) - AlgorithmHub

---
+++ Leetcode/Medium/0008. String to Integer (atoi)/0008-string-to-integer-atoi.py
@@ -0,0 +1,32 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        # 1. Ignore leading whitespaces
+        position = 0
+        while position < len(s) and s[position] == " ":
+            position += 1
+
+        # 2. Determine signedness
+        sign = 1
+
+        if position < len(s) and s[position] == "-":
+            sign *= -1
+            position += 1
+        elif position < len(s) and s[position] == "+":
+            position += 1
+
+        # 3. Read actual digits
+        number = 0
+        while position < len(s) and s[position].isdigit():
+            number = number * 10 + int(s[position])
+            position += 1
+
+        # 4. Apply sign
+        final = number * sign
+
+        # 5. Apply range
+        if final > 2147483647:
+            final = 2147483647
+        elif final < -2147483648:
+            final = -2147483648
+
+        return final
